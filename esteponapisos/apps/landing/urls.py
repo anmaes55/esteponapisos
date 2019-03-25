02e59032659d798ac02c6d0423a53e705c9f8082
Juan <juan@quitiweb.com>
2019-03-25 21:47:59 +0100

    Añadidas nuevas html
    Se ha añadido callesestepona, orquidarioestepona, catalogo_4B, se ha dejado el index antiguo y se ha modificado el base.html

--- esteponapisos/apps/landing/urls.py
+++ esteponapisos/apps/landing/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('callesestepona', views.callesestepona, name='callesestepona'),
+    path('orquidarioestepona', views.orquidarioestepona, name='orquidarioestepona'),
+    path('catalogo_4B', views.catalogo_4B, name='catalogo_4B'),
 
 ]
--- esteponapisos/apps/landing/views.py
+++ esteponapisos/apps/landing/views.py
@@ -3,3 +3,17 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'landing/index.html')
+
+
+def callesestepona(request):
+    return render(request, 'landing/callesestepona.html')
+
+
+def orquidarioestepona(request):
+    return render(request, 'landing/orquidarioestepona.html')
+
+
+def catalogo_4B(request):
+    return render(request, 'landing/catalogo_4B.html')
+
+
